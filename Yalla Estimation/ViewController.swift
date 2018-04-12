244ee024237356694225cee48a09e39a04a8196f
amr <amrhym@icloud.com>
2018-04-12 20:29:26 +0300

    Initial Commit

---
+++ Yalla Estimation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Yalla Estimation
+//
+//  Created by amr Elshendidy on 4/12/18.
+//  Copyright © 2018 amr Elshendidy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
